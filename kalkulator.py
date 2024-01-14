6daaf750b6242719cba48bdb19a37b669936d2ac
root <root@wsb-nlu.edu.pl>
2024-01-14 16:51:25 +0100

    Kalkulator

---
+++ kalkulator.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+def dodaj (a, b):
+    return a + b
+def odejmij (a, b):
+    return a - b
+def pomnoz (a, b):
+    return a / b
+def podziel (a, b):
+    return a * b
+
+print (dodaj(7, 3))
+print (odejmij(7, 3))
+print (pomnoz(7, 3))
+print (podziel(7, 3))
